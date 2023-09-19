1f517eab73fd53eba84bc467306a46f5401ff77f
DMakiz <142487178+DMakiz@users.noreply.github.com>
2023-09-19 12:38:08 +0300

    Create model_list.py

---
+++ diffusion_webui/utils/model_list.py
@@ -0,0 +1,22 @@
+stable_model_list = [
+    "runwayml/stable-diffusion-v1-5",
+    "SG161222/Realistic_Vision_V2.0"
+]
+
+stable_inpiant_model_list = [
+    "stabilityai/stable-diffusion-2-inpainting",
+    "runwayml/stable-diffusion-inpainting",
+]
+
+controlnet_model_list = [
+    "lllyasviel/control_v11p_sd15_canny",
+    "lllyasviel/control_v11f1p_sd15_depth",
+    "lllyasviel/control_v11p_sd15_openpose",
+    "lllyasviel/control_v11p_sd15_scribble",
+    "lllyasviel/control_v11p_sd15_mlsd",
+    "lllyasviel/control_v11e_sd15_shuffle",
+    "lllyasviel/control_v11e_sd15_ip2p",
+    "lllyasviel/control_v11p_sd15_lineart",
+    "lllyasviel/control_v11p_sd15s2_lineart_anime",
+    "lllyasviel/control_v11p_sd15_softedge",
+]
